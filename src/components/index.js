fa9d2ad1ea021342279a3865cf90dc9acf04365a
Stefan Blazevski <45402103+stefanblazevski@users.noreply.github.com>
2024-09-01 15:44:35 +0200

    final folder structure

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
-export {default as Article} from './article/Artical';
 export {default as Brand} from './brand/Brand';
 export {default as CTA} from './cta/CTA';
 export {default as Feature} from './feature/Feature';
