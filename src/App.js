e6f98e3e4447435630098395ddff0a689ddbbb6f
Stefan Blazevski <45402103+stefanblazevski@users.noreply.github.com>
2024-09-04 17:42:39 +0200

    final version

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { Footer, Blog, Possibility, Features, Header, WhatGPT3} from './containers';
 import { CTA, Brand, Navbar } from './components';
+import './App.css';
 
 const App = () => {
   return (
